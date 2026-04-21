19f0ca094022ae8f101ead12e0fb2aca9ffed6b9
San <sganis@gmail.com>
2026-04-21 08:55:06 +0300

    New browser build

--- browser/public/_app/immutable/entry/start.CSdZ3FMt.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/C4OEdPbY.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.CSdZ3FMt.js.map
--- browser/public/_app/immutable/nodes/1.Bvj3RjXP.js
+++
@@ -1,2 +0,0 @@
-import{a as i,f as g,s as a}from"../chunks/D8g9b6Wm.js";import{n as v,o as d,t as l,q as _,u as o,v as p,w as x}from"../chunks/CZjVXtFS.js";import{s as $,p as m}from"../chunks/C4OEdPbY.js";const k={get error(){return m.error},get status(){return m.status}};$.updated.check;const n=k;var b=g("<h1> </h1> <p> </p>",1);function j(u,c){v(c,!0);var r=b(),t=d(r),f=o(t,!0);p(t);var e=x(t,2),h=o(e,!0);p(e),l(()=>{var s;a(f,n.status),a(h,(s=n.error)==null?void 0:s.message)}),i(u,r),_()}export{j as component};
-//# sourceMappingURL=1.Bvj3RjXP.js.map
