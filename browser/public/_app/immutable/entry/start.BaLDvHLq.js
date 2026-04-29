22f2620685aec35fc4622a5b6f042a624ea8ca6f
San <sganis@gmail.com>
2026-04-29 20:03:47 +0300

    Added build

---
+++ browser/public/_app/immutable/entry/start.BaLDvHLq.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/2K8Pn13Q.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.BaLDvHLq.js.map
--- browser/public/_app/immutable/entry/start.DmRM_Haa.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/CaYqoL-J.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.DmRM_Haa.js.map
